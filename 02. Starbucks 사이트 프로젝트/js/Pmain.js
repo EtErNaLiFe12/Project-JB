57ee8d01c0e88f1cd5639e4046218d93aafab379
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-04-10 17:30:39 +0900

    Update (21.04.10) - Review 내용 추가

--- 02. Starbucks 사이트 프로젝트/js/Pmain.js
+++ 02. Starbucks 사이트 프로젝트/js/Pmain.js
@@ -204,8 +204,134 @@ if(isShow) {
 }
 */
 
+// ----------------------------------------------------------------------------------------------------
+
 //DOM API - JAVASCRIPT에서 HTML을 제어할때 사용하는 명령 
 
+/*
 let boxEl = document.querySelector('.box'); //html상에 선언된 클래스를 boxEL에 할당하는 구문
 
 console.log(boxEl);
+
+boxEl.addEventListener('click', function () {
+  console.log('click!!');
+  boxEl.classList.add('active');
+  console.log(boxEl.classList.contains('active')
+  );
+  boxEl.classList.remove('active');
+  console.log(
+    boxEl.classList.contains('active')
+  );
+});
+*/
+
+/*
+//HTML 요소(Element) 1개 검색/찾기
+const boxEl = document.querySelector('.box');
+
+//HTML 요소에 적용할 수 있는 메소드!
+boxEl.addEventListener();
+
+//인수(Arguments)를 추가 가능!
+boxEl.addEventListener(1, 2);
+
+// 1-이벤트(Event, 상황)
+boxEl.addEventListener('click', 2);
+
+// 2-핸들러(Handler, 실행할 함수)
+boxEl.addEventListener('click', function() {
+  console.log('Click~!');
+}); 
+*/
+
+/*
+//요소의 클래스 정보 객체 활용!
+
+boxEl.classList.add('active');
+let isContains = boxEl.classList.contains('active');
+console.log(isContains);
+
+boxEl.classList.remove('active');
+isContains = boxEl.classList.contains('active');
+console.log(isContains); */
+
+/*
+//HTML 요소(Element) 모두 검색/찾기
+const boxEls = document.querySelectorAll('.box');
+console.log(boxEls);
+
+//찾은 요소들 반복해서 함수 실행!
+//익명 함수를 인수로 추가!
+boxEls.forEach(function () {});
+
+//첫 번째 매개변수(boxEl): 반복 중인 요소
+//두 번째 매개변수(index): 반복 중인 번호
+boxEls.forEach(function (boxEl, index) {});
+
+// 출력!
+boxEls.forEach (function (boxEl, index) {
+  boxEl.classList.add(`order-${index + 1}`);
+  console.log(index, boxEl)
+}) 
+
+//---------------------------------------------
+
+const boxEls = document.querySelectorAll('.box');
+
+boxEls.forEach(function (boxEl, index) {
+  boxEl.classList.add(`order-${index + 1}`)
+  console.log(index, boxEl );
+}); */
+
+/*
+const boxEl = document.querySelector('.box'); // box 클래스가 여러개일 경우 제일 먼저 찾아지는 부분을 반환함
+
+//Getter, 값을 얻는 용도
+console.log(boxEl.textContent);  // textContent - 문자 데이터
+
+//Setter, 값을 지정하는 용도
+boxEl.textContent = 'HEROPY!!';
+console.log(boxEl.textContent); */
+
+//------------------------------------------------------------------------------------
+/*
+//메소드 체이닝(Method Chaining)
+
+const a = 'Hello~';
+// split: 문자를 인수 기준으로 쪼개서 배열로 반환.
+// reverse: 배열을 뒤집기.
+// join: 배열을 인수 기준으로 문자로 병합해 반환.
+
+const b = a.split('').reverse().join(''); // 메소드 체이닝
+
+console.log(a);
+console.log(b);*/
+
+//------------------------------------------------------------------------------------
+
+// theQuickBrownFox - camel 표기법
+
+let fruits = ['Apple', 'Banana', 'Cherry'];
+console.log(fruits[1]);
+
+
+const hello = function () {
+  console.log('hello');
+}
+
+hello();
+ 
+let cham = true;
+let gugeet = false;
+
+if(cham) {
+  console.log(`${cham} 입니다.`);
+} else {
+  console.log(`${gugeet} 입니다.`)
+}
+
+const a = document.querySelector('.box');
+console.log(a.textContent);
+a.addEventListener('click', function () {
+  console.log('Hello~');
+});
