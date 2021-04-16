0c751985fc0ab7839ef9a4c2da5d6c0448f36485
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-04-16 13:04:45 +0900

    공통 모듈 분리

--- 02. Starbucks 사이트 프로젝트/js/main.js
+++ 02. Starbucks 사이트 프로젝트/js/main.js
@@ -1,23 +1,3 @@
-const searchEl = document.querySelector('.search'); // document도 하나의 요소 = html로 보면 된다.
-const searchInputEl = searchEl.querySelector('input') // 찾은 요소를 넣은 변수의 명으로 querySelector를 통해 찾을 수 있다.
-
-//검색창 이벤트 삽입
-searchEl.addEventListener('click', function () {
-  // Logic..
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 //배지 & 버튼 보임/숨김 이벤트 삽입
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -181,6 +161,3 @@ spyEls.forEach(function (spyEl) {
 });
 
 
-//년도 자동 출력 이벤트 추가
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2021
